db1762fb6d23a0aa6b6f4edc3e1a0850c525225f
hans11 <t5573@outlook.com>
2016-03-20 21:36:36 -0600

    Assignment 2 - Submission
    Submission of Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Tyler Becker - R Programming - Assignment 2
+## This function creates a special matrix that is loaded into memory for performance purposes
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+
 }
 
 
-## Write a short comment describing this function
+## This function takes the matrix that was loaded into memory and passes it through our solve function
+##
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+    i <- x$getinverse() 
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
+    }
+
